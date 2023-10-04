ce022e1df5dbb0770372b436c117a178bc22a9cf
Pedro H <pedro_gonsalves@hotmail.com>
2023-10-04 14:08:52 -0300

    add: solving my third leet code problem. easy level. finding the target value in sum of two elements in array

---
+++ src/main/scala/leetcode/two_sum.scala
@@ -0,0 +1,22 @@
+package leetcode
+
+object two_sum extends App {
+
+  val numsToExample: Array[Int] = Array(2, 7, 11, 15)
+  val target: Int = 9
+
+  val result = twoSum(numsToExample, target)
+  result.foreach(println)
+
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    val arrayToReturn = scala.collection.mutable.ArrayBuffer.empty[Int]
+    for {
+      i <- nums.indices
+      j <- i + 1 until nums.length
+      if nums(i) + nums(j) == target
+    } {
+      arrayToReturn += (i, j)
+    }
+    arrayToReturn.toArray
+  }
+}
