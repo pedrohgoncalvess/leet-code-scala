2232161e5e5c86ddb1afa489a48d6cc00f2e65a7
Pedro H <pedro_gonsalves@hotmail.com>
2023-10-04 14:06:23 -0300

    add: solving my first problem. easy level

---
+++ src/main/scala/leetcode/good_pairs.scala
@@ -0,0 +1,12 @@
+package leetcode
+
+object good_pairs extends App {
+
+  val nums: Array[Int] = Array(1,1,2,3,1,2,3)
+
+  val count = nums.indices.foldLeft(0) { (acc, i) =>
+    acc + nums.slice(i + 1, nums.length).count(_ == nums(i))
+  }
+
+  println(count)
+}
