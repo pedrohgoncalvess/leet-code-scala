514f250f2eab97a9b307d298bc1995b35bc99dc2
unknown <pedro_gonsalves@hotmail.com>
2024-01-08 21:07:24 -0300

    add: finished reverse_integer exercise

--- readme.md
+++ readme.md
@@ -35,5 +35,17 @@ Implement the MyHashMap class:
 - void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
 
 
+---
+
+**Title**: [Reverse Number](https://leetcode.com/problems/reverse-integer/submissions/1140929415/)
+
+**File**: reverse_integer.scala
+
+**Level**: medium
+
+**Explanation**:Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+
 
 
---
+++ src/main/scala/leetcode/reverse_integer.scala
@@ -0,0 +1,38 @@
+package leetcode
+
+object reverse_integer extends App{
+
+  def reverse(x: Int): Int = {
+
+    if (x == 0) {
+      return 0
+    }
+
+    val arrayCase = if (x.toString.toArray.last.toString == "0") x.toString.toArray.dropRight(1).reverse else x.toString.toArray.reverse
+
+    var finalString = if (x < 0) "-" else ""
+
+    for (elem <- arrayCase) {
+      try {
+      elem.toString.toInt
+      finalString = finalString.concat(elem.toString)
+      } catch {
+        case _:java.lang.NumberFormatException =>
+      }
+   }
+    val intToReturn: Option[Int] = finalString.toIntOption
+    if (intToReturn.isEmpty) 0 else intToReturn.get
+  }
+
+  val case1 = 123
+  val case2 = -123
+  val case3 = 120
+  val case4 = 0
+  val case5 = 1534236469
+
+  println(reverse(case1))
+  println(reverse(case2))
+  println(reverse(case3))
+  println(reverse(case4))
+  println(reverse(case5))
+}
